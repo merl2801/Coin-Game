1bcf9d3de88ea9a9170058ee4a3fc1f48caff4a0
merl2801 <landapda@gmail.com>
2025-09-09 22:08:35 +0700

    update file

--- .github/scripts/calculate.py
+++ .github/scripts/calculate.py
@@ -1,7 +1,9 @@
 import yaml
 
-with open("data.yml", "r") as f:
+
+with open(".github/ISSUE_TEMPLATE/data.yml", "r") as f:
     data = yaml.safe_load(f)
+
 
 numbers = data["numbers"]
 total = sum(numbers)
